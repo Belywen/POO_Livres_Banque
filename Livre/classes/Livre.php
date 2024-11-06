87291e3311682597e31e1a5dbc95c02e8bc7589f
Laury <renaulaury@gmail.com>
2024-11-06 18:04:06 +0100

    maj poo livre

--- Livre/classes/Livre.php
+++ Livre/classes/Livre.php
@@ -8,6 +8,46 @@ class Livres {
 
     public function __construct($titre) {
         $this->titre = $titre;
+        $this->auteur = $auteur;
+        $this->nbPages = $nbPages;
+        $this->anneeParution = $anneeParution;
+        $this->prix = $prix;
+    }
+
+    public function getTitre() {
+        return  $this->titre;
+    }
+
+    public function setTitre($titre) {
+        $this->titre = $titre;
+        return  $this;
+    }
+
+    public function getNbPages() {
+        return  $this->titre;
+    }
+
+    public function setNbPages($nbPages) {
+        $this->nbPages = $nbPages;
+        return  $this;
+    }
+
+    public function getAnneeParution() {
+        return  $this->anneeParution;
+    }
+
+    public function setAnneeParution($anneeParution) {
+        $this->anneeParution = $anneeParution;
+        return  $this;
+    }
+
+    public function getPrix() {
+        return  $this->prix;
+    }
+
+    public function setPrix($prix) {
+        $this->prix = $prix;
+        return  $this;
     }
 }
 

f2523c4d14f48257d65109c6e7a92d58452f2a38
Laury <112404773+Belywen@users.noreply.github.com>
2024-11-07 08:29:38 +0100

    maj entre

--- Entreprise/index.php
+++ Entreprise/index.php
@@ -15,12 +15,12 @@
 
     $elanFormation = new Entreprise("ELAN FORMATION", "1993-01-01", "14 rue du Rhône", "67100", "STRASBOURG");
     // var_dump($elanFormation);
-    // echo $elanFormation->getRaisonSociale()."a été crée le ".$elanFormation->getDateCreation()->format("d-M-Y");
+    echo $elanFormation->getRaisonSociale()."a été crée le ".$elanFormation->getDateCreation()->format("d-M-Y");
     echo $elanFormation."</br>";
-    // echo $elanFormation->getInfos()."</br>";
+    echo $elanFormation->getInfos()."</br>";
 
-    // $stephane = new Employe("SMAIL", "Stéphane", "stephane@elan-formation.fr")."</br>";
-    // echo $stephane;
+    $stephane = new Employe("SMAIL", "Stéphane", "stephane@elan-formation.fr")."</br>";
+    echo $stephane;
     
     ?>
 </body>
